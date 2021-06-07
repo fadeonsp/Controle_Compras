e91c5f6edae567d9c0ac5e78fd2d89891f72ccf4
edfadeon <edfadeon@gmail.com>
2021-06-07 17:27:06 -0300

    enviando arquivo pra github

---
+++ js/config.js
@@ -0,0 +1,8 @@
+function setConfig() {
+    var texts = {
+        "title": "Controle de Compras"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+setConfig();
